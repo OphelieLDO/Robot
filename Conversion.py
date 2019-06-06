12a4d4cd45a23396301b8de2cf4854497c959008
Laura Habert <33000829@u-paris10.fr>
2019-06-06 15:12:21 +0200

    mise en forme affichage

--- Conversion.py
+++ Conversion.py
@@ -10,14 +10,10 @@ from tkinter import filedialog
 from tkinter.filedialog import askopenfilename, askdirectory
 
 fenetre = Tk()
+fenetre.geometry("500x200")
 champ_label = Label(fenetre, text="Bienvenue!")
 # On affiche le label dans la fenêtre
 champ_label.pack()
-
-filename = StringVar(fenetre)
-
-entry = Entry(fenetre, textvariable=filename)
-entry.pack()
 
 
 # Fonction pour selectionner le fichier csv
@@ -25,36 +21,31 @@ def csv_fileSelect():
     filename.set(askopenfilename(filetypes=(("csv files", "*.csv"), ("all files", "*.*"))))
 
 
-bouton_choisir = Button(fenetre, text="Choisir le fichier csv", command=csv_fileSelect)
-bouton_choisir.pack()
+bouton_choisir_csv = Button(fenetre, text="Choisir le fichier csv", command=csv_fileSelect)
+bouton_choisir_csv.pack()
 
-var_texte_entree = StringVar()
-ligne_entree = Entry(fenetre, textvariable=var_texte_entree, width=30)
-ligne_entree.pack()
+filename = StringVar(fenetre)
+entryCSV = Entry(fenetre, textvariable=filename, width="80")
+entryCSV.pack()
 
 
-
-# Fonction pour selectionner le fichier csv
+# Fonction pour selectionner le dossier de destination
 def directorySelect():
-    filename.set(askdirectory)
+    directoryname.set(askdirectory(initialdir="/", title='Choisissez un repertoire'))
 
 
+bouton_choisir_directory = Button(fenetre, text="Choisir le dossier où mettre le .top généré", command=directorySelect)
+bouton_choisir_directory.pack()
 
-
-champ_fichier_sortie = Label(fenetre, text="Entrez le lien de votre dossier de destination:")
-# On affiche le label dans la fenêtre
-champ_fichier_sortie.pack()
-
-var_texte_sortie = StringVar()
-ligne_sortie = Entry(fenetre, textvariable=var_texte_sortie, width=30)
-ligne_sortie.pack()
+directoryname = StringVar(fenetre)
+entryDirectory = Entry(fenetre, textvariable=directoryname, width="80")
+entryDirectory.pack()
 
 bouton_lancer = Button(fenetre, text="Lancer", command=fenetre.quit)  # mettre la commande qui mènera au traitement
 bouton_lancer.pack()
 
 bouton_quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
 bouton_quitter.pack()
-
 
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
 fenetre.mainloop()
