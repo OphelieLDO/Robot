5c1d000037a35d19f272698f55a5273fa6fb53cc
ophel <ophelielopesdoliveira@hotmail.fr>
2019-06-06 12:27:49 +0200

    Modification du boutton pour choisir le dossier

--- Conversion.py
+++ Conversion.py
@@ -15,7 +15,7 @@ champ_label = Label(fenetre, text="Bienvenue!")
 champ_label.pack()
 
 filename = StringVar(fenetre)
-
+directoryname = StringVar(fenetre)
 entry = Entry(fenetre, textvariable=filename)
 entry.pack()
 
@@ -25,36 +25,23 @@ def csv_fileSelect():
     filename.set(askopenfilename(filetypes=(("csv files", "*.csv"), ("all files", "*.*"))))
 
 
-bouton_choisir = Button(fenetre, text="Choisir le fichier csv", command=csv_fileSelect)
-bouton_choisir.pack()
-
-var_texte_entree = StringVar()
-ligne_entree = Entry(fenetre, textvariable=var_texte_entree, width=30)
-ligne_entree.pack()
-
+bouton_choisir_csv = Button(fenetre, text="Choisir le fichier csv", command=csv_fileSelect)
+bouton_choisir_csv.pack()
 
 
 # Fonction pour selectionner le fichier csv
 def directorySelect():
-    filename.set(askdirectory)
+    directoryname.set(askdirectory())
 
 
-
-
-champ_fichier_sortie = Label(fenetre, text="Entrez le lien de votre dossier de destination:")
-# On affiche le label dans la fenêtre
-champ_fichier_sortie.pack()
-
-var_texte_sortie = StringVar()
-ligne_sortie = Entry(fenetre, textvariable=var_texte_sortie, width=30)
-ligne_sortie.pack()
+bouton_choisir_directory = Button(fenetre, text="Choisir le dossier où mettre le .top généré", command=directorySelect)
+bouton_choisir_directory.pack()
 
 bouton_lancer = Button(fenetre, text="Lancer", command=fenetre.quit)  # mettre la commande qui mènera au traitement
 bouton_lancer.pack()
 
 bouton_quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
 bouton_quitter.pack()
-
 
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
 fenetre.mainloop()
