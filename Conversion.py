2abd20845046cd0a52011f0ae4c7443b48068c59
ophel <ophelielopesdoliveira@hotmail.fr>
2019-06-11 15:08:25 +0200

    ajout de la creation du .top avec ajout des lignes brutes

--- Conversion.py
+++ Conversion.py
@@ -5,7 +5,9 @@
 import os
 from tkinter import *
 import csv
-from tkinter import filedialog
+import qi
+import argparse
+import sys
 
 # Création de la fenetre et titre
 from tkinter.filedialog import askopenfilename, askdirectory
@@ -20,6 +22,7 @@ champ_label.pack()
 # Fonction pour selectionner le fichier csv
 def csv_fileSelect():
     filename.set(askopenfilename(filetypes=(("csv files", "*.csv"), ("all files", "*.*"))))
+
 
 bouton_choisir_csv = Button(fenetre, text="Choisir le fichier csv", command=csv_fileSelect)
 bouton_choisir_csv.pack()
@@ -42,13 +45,14 @@ entryDirectory = Entry(fenetre, textvariable=directoryname, width="80")
 entryDirectory.pack()
 
 
-# Permet de convertir les ligne du csv
+# Permet de convertir les lignes du csv
 def replace_question_csv(question):
     print("replace_csv")
     # faire un replace pour convertir le csv en ligne .top
     return question
 
-# Permet de créer le .top avec le header qui est bon
+
+# Permet de créer le .top avec le header et les lignes du csv
 def create_top():
     # Creer le topic
     fichier = open(entryDirectory.get() + "/universite.top", "w")
@@ -59,13 +63,36 @@ def create_top():
         for row in reader:
             question = row[0]
             reponse = row[1]
-            fichier.write("\nu:("+replace_question_csv(question)+") "+replace_question_csv(reponse))
+            fichier.write("\nu:(" + replace_question_csv(question) + ") " + replace_question_csv(reponse))
             print(reponse)
 
     fichier.close()
 
 
-bouton_lancer = Button(fenetre, text="Lancer", command=create_top)  # mettre la commande qui mènera au traitement
+def load():
+    print("Transfert vers Nao")
+    ##Connection ftp
+    host = "169.254.129.162"  # adresse du serveur FTP
+    user = "nao"  # votre identifiant
+    password = "nao"  # votre mot de passe
+    connect = ftp.ftplib(host, user, password)  # on se connecte
+    commande = raw_input('CWD dialog')  # entrez la commande à effectuer
+    connect.sendcmd(commande)
+    ##Ouverture du fichier
+    fichier = entryDirectory.get() + "/universite.top"
+    file = open(fichier, 'rb')  # ici, j'ouvre le fichier ftp.py
+    connect.storbinary('STOR ' + fichier,
+                       file)  # ici (où connect est encore la variable de la connexion), j'indique le fichier à envoyer
+    file.close()  # on ferme le fichier
+    connect.quit()  # où "connect" est le nom de la variable dans laquelle vous avez déclaré la connexion !
+
+
+def traitement():
+    create_top()
+    load()
+
+
+bouton_lancer = Button(fenetre, text="Lancer", command=load)  # mettre la commande qui mènera au traitement
 bouton_lancer.pack()
 
 bouton_quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
